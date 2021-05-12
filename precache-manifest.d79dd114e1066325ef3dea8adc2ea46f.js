5e6d8d8b55e6d82cd7156b4a6aadd00ffc671c11
Code Sandbox <hello@codesandbox.io>
2021-05-12 10:28:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d79dd114e1066325ef3dea8adc2ea46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29681b4096ee944d3a4282b9200a9d7",
+    "url": "https://huynt-dev.github.io/csb-4cwqu/index.html"
+  },
+  {
+    "revision": "cc658459d83df276d147",
+    "url": "https://huynt-dev.github.io/csb-4cwqu/static/css/main.2b558751.chunk.css"
+  },
+  {
+    "revision": "dd1769ec1eb716d7899d",
+    "url": "https://huynt-dev.github.io/csb-4cwqu/static/js/2.df0a9ef8.chunk.js"
+  },
+  {
+    "revision": "cc658459d83df276d147",
+    "url": "https://huynt-dev.github.io/csb-4cwqu/static/js/main.96438002.chunk.js"
+  },
+  {
+    "revision": "30c1c411787b7b0a696e",
+    "url": "https://huynt-dev.github.io/csb-4cwqu/static/js/runtime~main.fa41579c.js"
+  }
+]);
